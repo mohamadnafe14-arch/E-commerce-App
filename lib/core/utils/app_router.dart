893dd7c7cd30b306931ddfe84555eb352f0321e6
mohamed <mnafe44@gmail.com>
2026-01-26 18:14:52 +0200

    update navigation

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,12 +1,47 @@
+import 'package:e_commerce_app/features/auth/presentation/views/register_view.dart';
+import 'package:e_commerce_app/features/auth/presentation/views/sign_up_view.dart';
+import 'package:e_commerce_app/features/cart/presentation/views/cart_view.dart';
+import 'package:e_commerce_app/features/categories/presentation/views/categories_view.dart';
+import 'package:e_commerce_app/features/checkout/presentation/views/checkout_view.dart';
+import 'package:e_commerce_app/features/products/presentation/views/products_view.dart';
 import 'package:e_commerce_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static const String initialRoute = '/';
-  static final router = GoRouter(routes: [
-    GoRoute(
-      path: initialRoute,
-      builder: (context, state) => const SplashView(),
-    ),
-  ]);
+  static const String categoriesRoute = '/categories';
+  static const String productsRoute = '/products';
+  static const String cartRoute = '/cart';
+  static const String regsiterRoute = '/register';
+  static const String signUpRoute = '/signUp';
+  static const String checkoutRoute = '/checkout';
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: initialRoute,
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: categoriesRoute,
+        builder: (context, state) => const CategoriesView(),
+      ),
+      GoRoute(
+        path: productsRoute,
+        builder: (context, state) => const ProductsView(),
+      ),
+      GoRoute(path: cartRoute, builder: (context, state) => const CartView()),
+      GoRoute(
+        path: checkoutRoute,
+        builder: (context, state) => const CheckoutView(),
+      ),
+      GoRoute(
+        path: regsiterRoute,
+        builder: (context, state) => const RegisterView(),
+      ),
+      GoRoute(
+        path: signUpRoute,
+        builder: (context, state) => const SignUpView(),
+      ),
+    ],
+  );
 }
